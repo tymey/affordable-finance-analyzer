5df5cbf906bb89468a798b23c2cada0bcb302f42
tymey <tyler.meyer.a@gmail.com>
2025-05-24 00:00:37 -0500

    feat: Convert to Flask web app, deliver same results for now

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+from model.inputs import get_project_inputs
+from model.lihtc_calculator import calculate_lihtc_equity_extended
+from model.capital_stack import build_advanced_capital_stack
+from model.cashflow_model import project_cash_flows_enhanced
+from model.utils import calculate_irr, calculate_dscr
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        # For now, use static inputs
+        inputs = get_project_inputs()
+
+        lihtc_info = calculate_lihtc_equity_extended(
+            eligible_basis=inputs["eligible_basis"],
+            applicable_fraction=inputs["applicable_fraction"],
+            credit_rate=inputs["credit_rate"],
+            pricing=inputs["pricing"],
+            credit_type=inputs["credit_type"],
+            include_syndication_fee=inputs["include_syndication_fee"],
+            syndication_fee_percent=inputs["syndication_fee_percent"],
+            use_bridge_loan=inputs["use_bridge_loan"],
+            bridge_loan_interest=inputs["bridge_loan_interest"],
+            bridge_loan_term_years=inputs["bridge_loan_term_years"]
+        )
+
+        capital_stack = build_advanced_capital_stack(inputs, lihtc_info["Net Equity After Fees"])
+
+        cash_flows = project_cash_flows_enhanced(
+            initial_noi=inputs["noi_year_1"],
+            noi_growth_rate=inputs["noi_growth_rate"],
+            debt_service=capital_stack["Loan (DSCR & LTV Constrained)"] * inputs["permanent_loan_rate"],
+            hold_period=inputs["hold_period"],
+            exit_cap_rate=inputs["exit_cap_rate"],
+            selling_cost_percent=inputs["selling_cost_percent"],
+            include_sale=True
+        )
+
+        irr = calculate_irr(cash_flows, capital_stack["Equity Required"])
+        dscr = calculate_dscr(inputs["noi_year_1"], capital_stack["Loan (DSCR & LTV Constrained)"] * inputs["permanent_loan_rate"])
+
+        return render_template("results.html",
+                               capital_stack=capital_stack,
+                               cash_flows=cash_flows,
+                               irr=irr,
+                               dscr=dscr,
+                               lihtc_info=lihtc_info)
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
