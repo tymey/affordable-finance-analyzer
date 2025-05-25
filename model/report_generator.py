0815267f7d32e19076509651821db37c7971ada2
tymey <tyler.meyer.a@gmail.com>
2025-05-24 20:54:54 -0500

    fix: Bug found with writing dicts & fixed with loop

--- model/report_generator.py
+++ model/report_generator.py
@@ -2,6 +2,7 @@ import xlsxwriter
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 import os
+import math
 
 def generate_excel_report(capital_stack, cash_flows, irr, dscr, filepath):
     workbook = xlsxwriter.Workbook(filepath)
@@ -12,15 +13,26 @@ def generate_excel_report(capital_stack, cash_flows, irr, dscr, filepath):
     row += 1
     for key, value in capital_stack.items():
         sheet.write(row, 0, key)
-        sheet.write(row, 1, value)
-        row += 1
+        if type(value) is dict:
+            for k, v in value.items():
+                sheet.write(row, 1, k)
+                sheet.write(row, 2, f"{v}")
+        else:
+            sheet.write(row, 1, f"{value}")
+            row += 1
     
     row += 1
     sheet.write(row, 0, "IRR")
-    sheet.write(row, 1, irr)
+    sheet.write(row, 1, "NaN%") if math.isnan(irr) else sheet.write(row, 1, irr)
     row += 1
     sheet.write(row, 0, "DSCR")
     sheet.write(row, 1, dscr)
+
+    # row += 2
+    # sheet.write(row, 0, "Add Test")
+    # sheet.write(row, 1, "=2+2")
+    # sheet.write(row, 2, f"=B{row + 1}+3")
+    # sheet.write(row, 3, f"=C{row + 1}+3")
 
     row += 2
     sheet.write(row, 0, "Annual Cash Flows")
@@ -44,10 +56,17 @@ def generate_pdf_report(capital_stack, cash_flows, irr, dscr, filepath):
     c.drawString(x, y, "Capital Stack")
     y -= 20
     for key, value in capital_stack.items():
-        c.drawString(x + 20, y, f"{key}: ${value:,.2f}")
-        y -= 15
+        if type(value) is dict:
+            c.drawString(x + 20, y, f"{key}:")
+            y -= 15
+            for k, v in value.items():
+                c.drawString(x + 40, y, f"{k}: ${v:,.2f}")
+                y -= 15
+        else:
+            c.drawString(x + 20, y, f"{key}: ${value:,.2f}")
+            y -= 15
     
-    y -= 10
+    y -= 15
     c.drawString(x, y, f"IRR: {irr}%")
     y -= 15
     c.drawString(x, y, f"DSCR: {dscr}")
